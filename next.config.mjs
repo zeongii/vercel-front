cf39a5f74f1fe54906a4d73d83cc74874cedbeae
yujeongoyj <oyj3989288@gmail.com>
2024-10-07 12:43:56 +0900

    app_key는env.local에 env.local은 .gitignore

--- next.config.mjs
+++ next.config.mjs
@@ -12,8 +12,16 @@ const nextConfig = {
     config.cache = false; // 캐시 비활성화
     return config;
   },
-  
+
+
+  env: {
+    // env는 서버측에서만 사용가능하기 때문에 next.config.js에서 
+    // 환경변수로 추가해주어야 클라이언트에서 접근 가능
+    KAKAO_MAP_APP_KEY: process.env.KAKAO_MAP_APP_KEY,
+  },
 };
+
+
 
 
 
